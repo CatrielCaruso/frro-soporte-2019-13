b8cdd7c6aedf243fdb6f3814db49b14659b30193
Catriel <catrielcaruso@gmail.com>
2019-06-03 11:41:24 -0300

    Completado hasta el ejercicio 2.

--- practico_04/ejercicio03.py
+++ practico_04/ejercicio03.py
@@ -1,4 +1,4 @@
 ## 3 Ejercicio Crear un Formulario que usando el control Treeview muestre la una lista con los nombre de
-## Ciudades Argentinas y su código postal ( por lo menos 5 ciudades ) . 
+## Ciudades Argentinas y su código postal ( por lo menos 5 ciudades ) .
 
 from tkinter import *
