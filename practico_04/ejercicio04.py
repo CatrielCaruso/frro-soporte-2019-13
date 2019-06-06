b9ea9de0f9d91c73ec2dd8d9150a57e367b0c7ef
Catriel <catrielcaruso@gmail.com>
2019-06-05 22:39:23 -0300

    Completado.

--- practico_04/ejercicio04.py
+++ practico_04/ejercicio04.py
@@ -2,75 +2,119 @@
 ## que inicia otra venta donde puedo ingresar una ciudad y su código postal .
 ## 2 – un botón Baja que borra del listad de ciudades la ciudad que esta selecionada en Treeview .
 ## 3 – un botón Modificar . Todos los cambios se deben ver reflejados en la lista que se muestra . 
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
 
 
+class Formulario(ttk.Frame):
+
+    def __init__(self, main_window):
+        super().__init__(main_window)
+        #Datos de la ventana principal
+        main_window.geometry("500x300")
+        main_window.title("Ciudades de Argentina")
+        main_window.iconbitmap("Screenshot_2019-06-03 UTN - Universidad Tecnologica Nacional Facultad Regional Rosario.ico")
+        main_window.config(cursor="watch")
+        main_window.config(bg="beige")
+         #datos del arbol(Treeview), Descarte que inicie desde ciudad porque no podia configurarlo en el alta.
+        self.treeview = ttk.Treeview(self)
+
+        item = self.treeview.insert("", tk.END, text="Rosario")
+        self.treeview.insert(item, tk.END, text=" CP 2000")
+        item = self.treeview.insert("", tk.END, text="Cordoba")
+        self.treeview.insert(item, tk.END, text="CP 5000")
+        item = self.treeview.insert("", tk.END, text="Chalten")
+        self.treeview.insert(item, tk.END, text="CP 9301")
+        item = self.treeview.insert("", tk.END, text="Ushuaia")
+        self.treeview.insert(item, tk.END, text="CP V9410")
+        item = self.treeview.insert("", tk.END, text="Funes")
+        self.treeview.insert(item, tk.END, text="CP S2122")
+        self.treeview.pack()
+
+        self.pack()
 
 
-class Listapais:
+main_window = tk.Tk()
+form = Formulario(main_window)
 
-     def __init__(self):
-
-        #Creo la ventana
-
-        self.ventana=Tk()
-
-
-        self.ventana.title("Ciudades y su código postal de Argentina")
-        self.ventana.iconbitmap("Screenshot_2019-06-03 UTN - Universidad Tecnologica Nacional Facultad Regional Rosario.ico")
-        self.ventana.config(cursor="watch")
-        self.ventana.config(bg="beige")
-        self.ventana.geometry("500x350")
-
-
-
-        #Creo el Arbol
-        self.arbol=ttk.Treeview(self.ventana)
-        self.arbol.pack( )
-        # Creo botones
-        self.ancho_boton = 11
-        self.alto_boton = 3
-        self.boton1 = Button(self.ventana,width=self.ancho_boton,height=self.alto_boton, text="Insert")
-        self.boton1.place(x=400,y=10)
-        self.boton2 = Button(self.ventana,width=self.ancho_boton,height=self.alto_boton, text="Delete")
-        self.boton2.place(x=400, y=135)
-        self.boton3 = Button(self.ventana,width=self.ancho_boton,height=self.alto_boton, text="Update")
-        self.boton3.place(x=400, y=260)
-
-        #Cargo el arbol
-        self.arbol.insert('', 'end', 'item0', text='Ciudades')
-
-        self.arbol.insert('item0','0','item1',text='Rosario')
-        self.arbol.insert('item0', '1', 'item2', text='Cordoba')
-        self.arbol.insert('item0', '2', 'item3', text='Chalten')
-        self.arbol.insert('item0', '3', 'item4', text='Ushuaia')
-        self.arbol.insert('item0', '4', 'item5', text='Funes')
-
-        self.arbol.insert('item1','end','dos1',text='CP 2000')
-        self.arbol.insert('item2', 'end', 'dos2', text='CP 5000 ')
-        self.arbol.insert('item3', 'end', 'dos3', text='CP 9301')
-        self.arbol.insert('item4', 'end', 'dos4', text='CP V9410')
-        self.arbol.insert('item5', 'end', 'dos5', text='CP S2132')
-        self.arbol.config(height=15)
+#funciones
+'''Alta'''
+def formulario_alta():
+    new_ventana = tk.Toplevel(main_window)
+    main_window.iconify()
+    var_ciudad = tk.StringVar()
+    var_CP = tk.StringVar()
+    label = tk.Label(new_ventana, text="Ciudad: ")
+    label2 = tk.Label(new_ventana, text="Código Postal: ")
+    label.grid(column=1, row=1, padx=(50,50), pady=(10,10))
+    label2.grid(column=2, row=1, padx=(40,40), pady=(10,5))
 
 
+    Ciudad = tk.Entry(new_ventana,textvariable=var_ciudad)
+    Ciudad.grid(column=1, row=2, padx=(5,5), pady=(10,5))
+    CP = tk.Entry(new_ventana,textvariable=var_CP)
+    CP.grid(column=2, row=2, padx=(5,5), pady=(10,5))
 
-        self.ventana.mainloop()
+    btn_alta = tk.Button(new_ventana, text="Añadir")
+    btn_alta.grid(column=3, row=2, padx=(50,50), pady=(10,10))
+    def alta(event):
+        item = form.treeview.insert("", tk.END, text=Ciudad.get())
+        form.treeview.insert(item, tk.END, text=CP.get())
+        main_window.deiconify()
 
+    btn_alta.bind("<Button-1>", alta)
 
-#Verificar
-def main():
-   mi_app = Listapais()
-   return 0
-
-
-if __name__ == '__main__':
-   main()
-
-
-
+ancho_boton = 11
+alto_boton = 3
+btn_frm_alta = tk.Button(main_window, bg='DarkSeaGreen1', text="ALTA",width=ancho_boton,height=alto_boton, command=formulario_alta)
+btn_frm_alta.place(x=400,y=80)
 
 
 
+'''Baja'''
+def baja():
+    dev_item = form.treeview.focus()
+    form.treeview.delete(dev_item)
+
+
+ancho_boton = 11
+alto_boton = 3
+btn_baja = tk.Button(main_window, bg='DarkSeaGreen1', text="BAJA",width=ancho_boton,height=alto_boton, command=baja)
+btn_baja.place(x=400,y=0)
+
+
+'''Modificación'''
+def formulario_modificacion():
+    new_ventana = tk.Toplevel(main_window)
+    main_window.iconify()
+    elem = form.treeview.focus()
+    elem_child = form.treeview.get_children(elem)
+    var_ciudad = tk.StringVar(new_ventana, value=form.treeview.item(elem)['text'])
+
+    var_CP = tk.StringVar(new_ventana, value=form.treeview.item(elem_child)['text'])
+    label = tk.Label(new_ventana, text="Ciudad: ")
+    label2 = tk.Label(new_ventana, text="Código Postal: ")
+    label.grid(column=1, row=1, padx=(50,50), pady=(10,10))
+    label2.grid(column=2, row=1, padx=(40,40), pady=(10,5))
+
+
+    Ciudad = tk.Entry(new_ventana,textvariable=var_ciudad)
+    Ciudad.grid(column=1, row=2, padx=(5,5), pady=(10,5))
+    CP = tk.Entry(new_ventana,textvariable=var_CP)
+    CP.grid(column=2, row=2, padx=(5,5), pady=(10,5))
+
+    btn_alta = tk.Button(new_ventana, text="Modificar")
+    btn_alta.grid(column=8, padx=(50,50), pady=(10,10), row=2)
+    def modificacion(event):
+        form.treeview.item(elem,text=Ciudad.get())
+        form.treeview.item(elem_child,text=CP.get())
+        main_window.deiconify()
+
+    btn_alta.bind("<Button-1>", modificacion)
+ancho_boton = 11
+alto_boton = 3
+btn_frm_alta = tk.Button(main_window, bg='DarkSeaGreen1', text="MODIFICAR",width=ancho_boton,height=alto_boton, command=formulario_modificacion)
+btn_frm_alta.place(x=400,y=170)
+
+form.mainloop()
